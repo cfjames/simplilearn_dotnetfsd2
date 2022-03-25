fc1b880420171407ef9790dc505c879ea8b9576f
James Ring-Howell <jringhowell@CoralFire.com>
2022-03-25 16:02:04 -0400

    Examples 3_25_22

---
+++ Simplilearn2/Phase1Section3.4/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+DoApp();
+
+void DoApp()
+{
+    bool xbool = true;
+    byte xbyte = 255;
+    sbyte xsbyte = -100;
+    char xchar = 'a';
+    decimal xdecimal = 2.3M;
+    double xdouble = 2.3;
+    float xfloat = 2.3f;
+    int xint = 1;
+    uint xuint = 90;
+    long xlong = 6;
+    ulong xulong = 5;
+    object xobject = new object();
+    short xshort = 13;
+    ushort xushort = 14;
+    string xstring = "name";
+
+
+}
