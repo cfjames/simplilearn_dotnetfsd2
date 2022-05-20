f413047bf69abb8c9fefb9f20b39792e30fc3eaf
James Ring-Howell <jringhowell@CoralFire.com>
2022-05-20 12:31:41 -0400

    Examples 4_29_22

---
+++ Simplilearn2/Phase1Section7.1/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Newtonsoft.Json.Linq;
+using Phase1Section7._9JRH;
+
+DoApp();
+
+void DoApp()
+{
+    //[{some jason object}]
+    JArray array = new JArray();
+    JValue text = new JValue("Manual text");
+    JValue date = new JValue(new DateTime(2022, 5, 23));
+
+    array.Add(text);
+    array.Add(date);
+
+    string json = array.ToString();
+    Console.WriteLine(json);
+
+    MyPackage myPck = new MyPackage();
+    myPck.DisplayText("Hello Class");
+    myPck.DisplayCurrentJsonDate();
+}
---
+++ Simplilearn2/Phase1Section7.9JRH/MyPackage.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace Phase1Section7._9JRH
+{
+    public class MyPackage
+    {
+        public void DisplayText(string text)
+        { 
+            Console.WriteLine(text);
+        }
+
+        public void DisplayCurrentDate()
+        {
+            Console.WriteLine(DateTime.Now.ToShortDateString());
+        }
+
+        public void DisplayCurrentJsonDate()
+        {
+            JValue date = new JValue(new DateTime(2022, 5, 23));
+            Console.WriteLine(date.ToString());
+        }
+    }
+}
