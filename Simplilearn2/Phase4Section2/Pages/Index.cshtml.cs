c187c1494b8a011bca442ff209297d38bfbcecd2
James Ring-Howell <jringhowell@CoralFire.com>
2022-08-24 13:51:17 -0400

    Example 1 8/24/22

---
+++ Simplilearn2/Phase4Section2/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Phase4Section2.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task OnGet()
+        {
+            using (var client = new HttpClient())
+            {
+                var request = new HttpRequestMessage();
+                request.RequestUri = new Uri("http://Phase4Section2a/Counter");
+                var response = await client.SendAsync(request);
+                string counter = await response.Content.ReadAsStringAsync();
+                ViewData["Message"] = $"Counter value from cache: {counter}";
+            }
+        }
+    }
+}
---
+++ Simplilearn2/Phase4Section2a/Controllers/CounterController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.Extensions.Caching.Distributed;
+using StackExchange.Redis;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Phase4Section2a.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CounterController : ControllerBase
+    {
+        private readonly ILogger<CounterController> _logger;
+        private readonly IDistributedCache _cache;
+
+        public CounterController(ILogger<CounterController> logger, IDistributedCache cache)
+        { 
+            _logger = logger;
+            _cache = cache;
+        }
+
+        [HttpGet(Name = "GetCounter")]
+        public string Get()
+        {
+            string key = "Counter";
+            string result = null;
+
+            try
+            {
+                var counterStr = _cache.GetString(key);
+                if (int.TryParse(counterStr, out int counter))
+                    counter++;
+                else
+                    counter = 0;
+
+                result = counter.ToString();
+                _cache.SetString(key, result);
+            }
+            catch (RedisConnectionException)
+            {
+                result = "Redis cache is not found.";
+            }
+
+            return result;
+        }
+    }
+}
